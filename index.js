296810cef907040bae64a25699adf87ff0f176f2
TylerLincoln <112194035+TylerLincoln@users.noreply.github.com>
2023-05-09 13:18:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function main() {
 	const WAIT_TIME_MS = 2000;
-	const offerButtons = document.querySelectorAll('button.btn.offer-cta');
-	for (let i = 0; i < offerButtons.length; ++i) {
+	const offerButtons = document.querySelectorAll('button.offer-cta');
+	for (let i = 2; i < offerButtons.length; ++i) {
 		setTimeout(() => {
 			try {
 				console.log('Clicking ' + offerButtons[i].id);
